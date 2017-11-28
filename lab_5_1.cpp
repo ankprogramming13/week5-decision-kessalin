3bae68baf5b7cdd490cedb45cb272bbf52451127
Unknown <kessalin2560@gmail.com>
2017-11-28 17:40:07 +0700

    ข้อที่1

--- lab_5_1.cpp
+++ lab_5_1.cpp
@@ -15,9 +15,11 @@ int main ()
     // รับชื่อที่ 2
     cout << "Enter the second name: ";
     cin >> name2;
+    if (name1==name2) {
 
     // เขียนเงื่อนไขให้ถูกต้องซะ หนูๆ
     cout << "The two names are equal" << endl;
+    } else {
     cout << "The two names are not equal" << endl;
 
     return 0;
