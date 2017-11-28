f366a24366e4fde0356011f8ff279971741362f2
Unknown <kessalin2560@gmail.com>
2017-11-28 18:03:05 +0700

    ข้อที่4

--- lab_5_4.cpp
+++ lab_5_4.cpp
@@ -11,6 +11,10 @@ int main() {
 
     switch (menu) {
         case 1: cout << "Call Program 1";
+        case 2: cout << "Call Program 2";
+        case 3: cout << "Call Program 3";
+        case 4: cout << "Call Program 4";
+        case 5: cout << "Call Program 5";
     }
 
     return 0;
